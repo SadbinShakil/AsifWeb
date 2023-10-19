679769218d7240de3f1134cd8e304f7fa2a81ef4
Sakil Sarker <61023087+SadbinShakil@users.noreply.github.com>
2023-10-19 15:06:56 +0600

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -25,7 +25,7 @@ const handleContactClick = () => {
       <NavbarContainer>
         <NavLogo to='/'>
           <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+            <DiCssdeck size="3rem" /> <Span>Asifuzzaman</Span>
           </a>
         </NavLogo>
         <MobileIcon>
