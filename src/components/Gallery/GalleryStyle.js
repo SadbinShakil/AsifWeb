b325ee9842e8584e8565251cd4fe4ec490f8829d
Sakil Sarker <61023087+SadbinShakil@users.noreply.github.com>
2023-11-15 10:37:35 +0600

    Update GalleryStyle.js

--- src/components/Gallery/GalleryStyle.js
+++ src/components/Gallery/GalleryStyle.js
@@ -44,13 +44,13 @@ margin-top: 2px;
 `;
 
 export const Desc = styled.div`
-    font-size: 12px;
+    font-size: 14px;
     text-align: center;
     max-width: 600px;
     color: ${({ theme }) => theme.text_secondary};
     @media (max-width: 768px) {
         margin-top: 2px;
-        font-size: 12px;
+        font-size: 14px;
     }
 `;
 
